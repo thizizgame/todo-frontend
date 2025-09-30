c6994bd1730e9246a93e4a0ceebb1d8143837f73
25LP4949 25LP4949 <25LP4949@G301-CC0194.local>
2025-09-30 12:01:46 +0800

    frontend changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,42 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState<{ id: string, name: string }[]>([]);
+
+  async function createNewTask() {
+    await fetch("http://localhost:3000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "applicexation/json",
+      },
+      body: JSON.stringify({ name: newTask }),
+    });
+    laodTasks();
+    setNewTask("");
+  }
+  function laodTasks() {
+    fetch("http://localhost:3000/tasks").then((res) => res.json()).then((data) => {
+      setTasks(data);
+    });
+  }
+  useEffect(() => {
+    laodTasks();
+  }, [])
+
   return (
-    <div>
-      <button className="btn btn-dash p-15">Click Me</button>
+    <div className="m-auto w-[1000px] mt-10">
+      <div className="flex gap-3">
+        <input className="input" value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+        <button className="btn btn-secondary rounded-xl" onClick={createNewTask}>Add</button>
+      </div>
+      {tasks.map((task) => (
+        <div className="card mt-4 p-4 border border-amber-100" key={task.id}>
+          {task.name}
+        </div>
+      ))}
     </div>
   );
 }
